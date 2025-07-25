f9d71afe0ea6b3aee5dd0f64d07364a30e07d4b9
Exprezzzor <132411298+Exprezzzo@users.noreply.github.com>
2025-07-25 10:26:14 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 // postcss.config.js
-// Corrected format for Next.js 14.2.5 - uses 'plugins' as an object.
-// This format should be fully compatible and resolve PostCSS errors for Next.js 14.x.
-
 module.exports = {
-  plugins: { // This format is correct for Next.js 14.x
+  plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
