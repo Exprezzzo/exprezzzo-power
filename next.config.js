669efc50b6c0fa27d01a98c1f4dfd015d8454bc0
jaydev <sbejayp@gmail.com>
2025-08-26 15:21:59 -0700

    fix: Resolve all merge conflicts (tsconfig, next.config)

--- next.config.js
+++ next.config.js
@@ -1,35 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: { 
     domains: ['firebasestorage.googleapis.com'],
     unoptimized: process.env.NODE_ENV === 'development'
   },
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin']
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          }
-        ],
-      },
-    ]
-  }
+  serverExternalPackages: ['firebase-admin']
 }
 
 module.exports = nextConfig
